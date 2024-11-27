dcad0703b20eb516f674359b2844f3c01f7a497f
rashiinfotech <151115705+rashiinfotech@users.noreply.github.com>
2024-11-27 21:47:04 +0530

    first commit

---
+++ wp-content/litespeed/js/d8bd77522e9ee0271c4332cd2237daa1.js
@@ -0,0 +1,4 @@
+(function($){function ihotspotInit(){$('.ihotspot_hastooltop').each(function(){$(this).data('powertip',function(){var htmlThis=$(this).parents('.ihotspot_tooltop_html').attr('data-html');return htmlThis});var thisPlace=$(this).parents('.ihotspot_tooltop_html').data('placement');$(this).powerTip({placement:thisPlace,smartPlacement:!0,mouseOnToPopup:!0,}).on({powerTipClose:function(){$('#powerTip').html('')}})})}
+$('body').on('click','.close_ihp',function(){$.powerTip.hide()});$(document).ready(function(){ihotspotInit()});let firstLoad=!0;function scroll_element(){let $top=$(window).scrollTop();if($top>=100&&firstLoad){ihotspotInit();firstLoad=!1}}
+$(window).scroll(function(){scroll_element()});$(window).bind('touchmove',function(e){scroll_element()})})(jQuery)
+;
